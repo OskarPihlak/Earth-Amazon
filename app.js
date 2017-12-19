c074c124057fcd69f0629c5e6409de1d17df9c44
Oskar Pihlak <oskar.pihlak@gmail.com>
2017-12-19 16:50:54 +0200

    Oids from db, refactoring and improving performance

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const exphbs = require('express-handlebars');
 const snmp = require("net-snmp");
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const Handlebars = require('handlebars');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 const printer_data = require('./assets/js/printer-data.js');
 const printer_data_promise = require('./assets/js/printer-data-promise.js');
+i = 0;
+
+function concatStyle(response, style){
+    for(let i= 0; i< response.length; i++){
+        if (response[i].name === style[i].name){
+            response[i].style = style[i];
+            console.log(i);
+            console.log(style[i]);
+        }
+    }
+    return response;
+}
 
 //create connection
 let db = mysql.createConnection({
@@ -36,64 +49,88 @@ app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
 
+Handlebars.registerHelper('ifEquals', function(a, b, options) {
+    if (a === b) {
+        return options.fn(this);
+    }
 
+    return options.inverse(this);
+});
+let temporary_file_array = [];
 app.get('/', function (req, res) {
     printer_data_promise().then(response =>{
         let sql_statement_get = 'SELECT * FROM inc_supply_status';
         let query = db.query(sql_statement_get, function (error, sql_data) {
-            let result = [];
-            for (let i = 0; i < sql_data.length; i++) {
-
-                let color_name_array = sql_data[i].cartridge_name;
-                let color_named = color_name_array.split(" ");
-                let color_name = color_named[0];
-                result.push ({
-                    id: sql_data[i].id,
-                    name: sql_data[i].printer_name,
-                    supply: sql_data[i].cartridge_supply,
-                    cartridge_class: sql_data[i].cartridge_name
-                });
-            }
-
-for(let i =0; i< response.length; i++){
-console.log(i);
-                for(let y=0; y< result.length; y++){
-console.log(y);
-                    if(response[i].name === result[y].name){
-                        response[i].cartridge[Object.keys(response[i].cartridge)[y]] = result[y];
-                        //console.log();
-                        //console.log(response[i]);
-                        //console.log(result[y]);
-                    }
-                }}
-              //  console.log(JSON.stringify(response[0]));
-
-
-
-            //console.log(response[1]);
-
-/*
-           // console.log(response[0].cartridge);
-            for(let y=0; y<response.length -1; y++){
-                console.log();
-                   for(let x=0 ;x< Object.keys(response[y].cartridge).length; x++){
-
-                       let sql_statement_get_x = 'SELECT * FROM printers_inc_supply.inc_supply_status WHERE printer_name="'+ response[y].name+'" AND cartridge_name="'+response[y].cartridge[Object.keys(response[y].cartridge)[x]].name +'"';
-                       let query = db.query(sql_statement_get_x, function (error, sql_data) {
-
-                           response[y].cartridge[Object.keys(response[y].cartridge)[x]] = sql_data[0].cartridge_supply;
-                    });
-                }
-            }
-            console.log(response[0].cartridge[Object.keys(response[0].cartridge)[0]]);
-            console.log(JSON.stringify(response[0]));
-
             if (error) throw error;
+            console.log(response.length);
+            let i = 0;
+
             res.render('main', {
-                printers: response,
-                inc_supply: result
-            });*/
+                printers: response
+            });
         });
+    }).catch(error => {console.log(error)});
+});
+result=[];
+app.get('/12k/:id', function (req, res) {
+    printer_data_promise("WHERE floor = '12k'").then(response => {
+        console.log(response);
+        res.render('twelf-floor', {
+            printer_name: req.params.id,
+            printers_12k:response
+
+        });
+    });
+});
+app.get('/10k/:id', function (req, res) {
+    printer_data_promise("WHERE floor = '10k'").then(response => {
+        res.render('tenth-floor', {
+            printer_name: req.params.id,
+            printers_10k:response
+        });
+    });
+});
+
+app.get('/6k/:id', function (req, res) {
+
+    res.render('sixth-floor', {
+        printer_name: req.params.id
+    });
+});
+
+app.get('/4k/:id', function (req, res) {
+
+    res.render('fourth-floor', {
+        printer_name: req.params.id
+    });
+});
+
+app.get('/3k/:id', function (req, res) {
+
+    res.render('third-floor', {
+        printer_name: req.params.id
+    });
+});
+
+app.get('/2k/:id', function (req, res) {
+
+    res.render('second-floor', {
+        printer_name: req.params.id
+    });
+});
+
+app.get('/1k/:id', function (req, res) {
+
+    res.render('first-floor', {
+        printer_name: req.params.id,
+        printer_data:[{name:''}]
+    });
+});
+
+app.get('/floors', function (req, res) {
+
+    res.render('floor-system', {
+        //printer_name: req.params.id
     });
 });
 
@@ -106,7 +143,12 @@ app.post('/', urlEncodedParser, function (req, res) {
     });
 });
 
-app.set('port', (process.env.PORT) || 3001);
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
+
+app.set('port', (process.env.PORT) || 3000);
 app.listen(app.get('port'), function () {
-    console.log('Server started on port' + app.get('port'))
+    console.log('Server started on port ' + app.get('port'))
 });
