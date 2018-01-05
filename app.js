0f2f10c6849b31753c373c82ae2762170692a57c
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-05 12:55:41 +0200

    added npm package

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ process.on('uncaughtException', function (err) {
     console.log(err);
 });
 
-app.set('port', (process.env.PORT) || 3000);
+app.set('port', (process.env.PORT) || 5000);
 app.listen(app.get('port'), function () {
     console.log('Server started on port ' + app.get('port'))
 });
