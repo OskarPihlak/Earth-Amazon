b316d656881a3768e541da1358ecfad92e082eb9
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-10 16:59:06 +0200

    db

--- assets/js/db.js
+++ assets/js/db.js
@@ -2,22 +2,25 @@ let mysql = require('mysql');
 
 
 module.exports.db_create_connection = ()=>{
-    db = mysql.createConnection({
+    db = mysql.createPool({
+        connectionLimit: 100,
         host: '127.0.0.1',
-        user: 'printers',
-        password: 'printers',
+        user: 'root',
+        password: '',
         database: 'printers_inc_supply',
         insecureAuth : true
     });
     return db;
 };
+exports.db_create_connection();
 
 module.exports.db_connect = ()=> {
-
     exports.db_create_connection();
-    db.connect(function (err) {
+    db.getConnection(function (err, connection){
+        console.log(connection);
         if (err) throw err;
         console.log('Mysql connected to printers_inc_supply on 127.0.0.1');
+        return connection;
     });
 };
 
