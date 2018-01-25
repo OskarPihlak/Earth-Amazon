bf9e6f1e7d5bb193a1568b1df1d76f669353dc1a
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-25 11:14:37 +0200

    fix

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const ActiveDirectory = require('activedirectory');
+//const ActiveDirectory = require('activedirectory');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 const methodOverride = require('method-override');
-const FBMessenger = require('fb-messenger');
+//const FBMessenger = require('fb-messenger');
 
 const printer_data_promise = require('./assets/js/printer-data-promise.js');
 const routing_get = require('./assets/js/routing-get.js');
@@ -18,7 +18,7 @@ let config = { url: 'ldap://dc.domain.com',
     baseDN: 'dc=domain,dc=com',
     username: 'username@domain.com',
     password: 'password' };
-let ad = new ActiveDirectory(config);
+//let ad = new ActiveDirectory(config);
 
 //app init
 let app = express();
@@ -46,8 +46,13 @@ routing_delete(app);
 process.on('uncaughtException', function (err) {
     console.log(err);
 });
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
 
 app.set('port', (process.env.PORT) || 80);
 app.listen(app.get('port'), function () {
     console.log('Server started on port ' + app.get('port'))
 });
+
