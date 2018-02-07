e7c48b033b2a99d52c01ce3f50f40d3d4e5e23d3
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-02-07 13:09:41 +0200

    fix

--- assets/js/db.js
+++ assets/js/db.js
@@ -4,8 +4,8 @@ module.exports.db_define_database = () => {
     pool = mysql.createPool({
         connectionLimit: 10,
         host: '127.0.0.1',
-        user: 'root',
-        password: '',
+        user: 'printers',
+        password: 'printers',
         database: 'printers_inc_supply',
         insecureAuth: true
     });
