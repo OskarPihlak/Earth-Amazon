03ed55fa12a66b6939189ed0a8b27cbcea75e2af
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-03-16 13:21:55 +0200

    fix

--- assets/js/db/db.js
+++ assets/js/db/db.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 
 module.exports.db_define_database = () => {
     pool = mysql.createPool({
-        connectionLimit: 10,
+        connectionLimit: 30,
         host: '127.0.0.1',
         user: 'printers',
         password: 'printers',
