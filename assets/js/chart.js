6c7fa73b561766c782b54c848653d30f9458d289
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-04-04 13:53:47 +0300

    test

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -54,97 +54,99 @@ module.exports = (printer_data_saved) => {
                     //add statistics
 
                     printer_data_saved.forEach(printer => {
-                        if(printer.printer_ping.alive) {
-                            printer.toner_graph = [];
-                            printer.xgrid = [];
-                            let data = [];
-                            data.value = [];
-                            data.dates = [];
-                            data.graph_data = [];
+                        if(printer.hasOwnProperty('printer_ping')) {
+                            if (printer.printer_ping.alive) {
+                                printer.toner_graph = [];
+                                printer.xgrid = [];
+                                let data = [];
+                                data.value = [];
+                                data.dates = [];
+                                data.graph_data = [];
 
-                            result.forEach(database_element => {
-                                printer.toner.forEach(toner => {
+                                result.forEach(database_element => {
+                                    printer.toner.forEach(toner => {
 
-                                    if (database_element.cartridge === toner &&
-                                        database_element.printer_name === printer.name &&
-                                        amount_of_days(70).contains(database_element.date)) {
+                                        if (database_element.cartridge === toner &&
+                                            database_element.printer_name === printer.name &&
+                                            amount_of_days(70).contains(database_element.date)) {
 
-                                        if (!data.dates.includes(moment(database_element.date).format('DD-MM-YYYY'))) data.dates.push(moment(database_element.date).format('DD-MM-YYYY'));
+                                            if (!data.dates.includes(moment(database_element.date).format('DD-MM-YYYY'))) data.dates.push(moment(database_element.date).format('DD-MM-YYYY'));
 
-                                        data.graph_data.push({
-                                            date: moment(database_element.date).format('DD-MM-YYYY'),
-                                            [database_element.color]: database_element.precentage,
-                                            [`toner_` + database_element.color]: database_element.cartridge
+                                            data.graph_data.push({
+                                                date: moment(database_element.date).format('DD-MM-YYYY'),
+                                                [database_element.color]: database_element.precentage,
+                                                [`toner_` + database_element.color]: database_element.cartridge
+                                            });
+                                        }
+                                    });
+                                });
+                                if (data.length = 0) {
+                                    console.log('data is empty')
+                                }
+
+                                //combine data per printer
+                                let printer_objects = [];
+                                printer_objects.printer_name = printer.name;
+                                printer_objects.ip = printer.ip;
+                                printer_objects.color = printer.color;
+
+                                data.dates.forEach(date => {
+                                    let single_date_object = [];
+                                    data.graph_data.forEach(toner_object => {
+                                        if (toner_object.date === date) {
+                                            toner_object.date = moment(date, 'DD-MM-YYYY').format('DD');
+                                            toner_object.date_big = moment(date, 'DD-MM-YYYY').format('DD-MM-YYYY');
+                                            single_date_object.push(toner_object);
+                                        }
+                                    });
+
+                                    let combined_single_date_object = {};
+                                    single_date_object.forEach(object => Object.assign(combined_single_date_object, object));
+                                    printer.toner_graph.push(combined_single_date_object);
+
+                                });
+
+                                //add week markers
+                                data.dates.forEach((date) => {
+                                    if (moment(date, 'DD-MM-YYYY').format('dddd') === 'Monday') printer.xgrid.push({
+                                        value: moment(date, 'DD-MM-YYYY').format('DD'),
+                                        text: `${(moment(date, 'DD-MM-YYYY').format('W'))} Nädal`
+                                    });
+                                });
+
+                                /*
+                                * pages printed
+                                * */
+
+
+                                let pages = [];
+                                printer.graph = [];
+
+                                //from pages-printed.sql
+                                pages_result.forEach(printed_pages => {
+                                    if (printer.ip === printed_pages.ip && moment(printed_pages.date).format('MM') === moment('2018-03-22').format('MM')) {
+                                        pages.push({
+                                            date: moment(printed_pages.date).format('DD-MM-YYYY'),
+                                            print_count: printed_pages.pages_printed
                                         });
                                     }
                                 });
-                            });
-                            if (data.length = 0) {
-                                console.log('data is empty')
-                            }
 
-                            //combine data per printer
-                            let printer_objects = [];
-                            printer_objects.printer_name = printer.name;
-                            printer_objects.ip = printer.ip;
-                            printer_objects.color = printer.color;
-
-                            data.dates.forEach(date => {
-                                let single_date_object = [];
-                                data.graph_data.forEach(toner_object => {
-                                    if (toner_object.date === date) {
-                                        toner_object.date = moment(date, 'DD-MM-YYYY').format('DD');
-                                        toner_object.date_big = moment(date, 'DD-MM-YYYY').format('DD-MM-YYYY');
-                                        single_date_object.push(toner_object);
+                                //generates graph data for page_print
+                                if (pages.length > 0) {
+                                    for (let i = 0; i < pages.length; i++) {
+                                        printer.graph.push({
+                                            pages_printed: parseInt(pages[i].print_count) - parseInt(pages[0].print_count),
+                                            date: pages[i].date
+                                        });
                                     }
-                                });
-
-                                let combined_single_date_object = {};
-                                single_date_object.forEach(object => Object.assign(combined_single_date_object, object));
-                                printer.toner_graph.push(combined_single_date_object);
-
-                            });
-
-                            //add week markers
-                            data.dates.forEach((date) => {
-                                if (moment(date, 'DD-MM-YYYY').format('dddd') === 'Monday') printer.xgrid.push({
-                                    value: moment(date, 'DD-MM-YYYY').format('DD'),
-                                    text: `${(moment(date, 'DD-MM-YYYY').format('W'))} Nädal`
-                                });
-                            });
-
-                            /*
-                            * pages printed
-                            * */
-
-
-                            let pages = [];
-                            printer.graph = [];
-
-                            //from pages-printed.sql
-                            pages_result.forEach(printed_pages => {
-                                if (printer.ip === printed_pages.ip && moment(printed_pages.date).format('MM') === moment('2018-03-22').format('MM')) {
-                                    pages.push({
-                                        date: moment(printed_pages.date).format('DD-MM-YYYY'),
-                                        print_count: printed_pages.pages_printed
-                                    });
+                                    printer.pages_printed_in_month = parseInt(pages[pages.length - 1].print_count) - parseInt(pages[0].print_count);
+                                } else {
+                                    console.log(colors.red('print_count is empty array'))
                                 }
-                            });
 
-                            //generates graph data for page_print
-                            if (pages.length > 0) {
-                                for (let i = 0; i < pages.length; i++) {
-                                    printer.graph.push({
-                                        pages_printed: parseInt(pages[i].print_count) - parseInt(pages[0].print_count),
-                                        date: pages[i].date
-                                    });
-                                }
-                                printer.pages_printed_in_month = parseInt(pages[pages.length - 1].print_count) - parseInt(pages[0].print_count);
-                            } else {
-                                console.log(colors.red('print_count is empty array'))
+                                printer_page_source.push(printer);
                             }
-
-                            printer_page_source.push(printer);
                         }
                     });
 
