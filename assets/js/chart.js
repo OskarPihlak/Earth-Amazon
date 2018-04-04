9585b2666c373905608480d31f9a3919fe03edc2
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-04-04 11:13:54 +0300

    test

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -18,8 +18,9 @@ module.exports = (printer_data_saved) => {
 
                     //add "toner" array element to object
                     for (let i = 0; i < printer_data_saved.length; i++) {
+                        console.log(printer_data_saved[i]);
                         printer_data_saved[i].toner = [];
-                        printer_data_saved[i].toner.push(printer_data_saved[i].cartridge[printer_oid_data.colors_loop_info()[0].inc_name].name);
+                        printer_data_saved[i].toner.push(printer_data_saved[i].cartridge['black'].name);
                         if (printer_data_saved[i].color) {
                             for (let x = 1; x < 4; x++) {
                                 printer_data_saved[i].toner.push(printer_data_saved[i].cartridge[printer_oid_data.colors_loop_info()[x].inc_name].name);
